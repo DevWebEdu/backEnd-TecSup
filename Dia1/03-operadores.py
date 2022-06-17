97e30c81c04b127190ac19cd3af09304e5631005
DevWebEdu <85375540+DevWebEdu@users.noreply.github.com>
2022-06-16 23:08:10 -0500

    files python

--- Dia1/03-operadores.py
+++ Dia1/03-operadores.py
@@ -9,3 +9,57 @@ print(edad_juan-edad_maria)
 print(edad_juan*edad_maria)
 #division
 print(edad_juan/edad_maria)
+#Modulo resultado entero de la division 
+print(edad_juan%edad_maria)
+#Cociente
+print(edad_juan//edad_maria)
+
+
+#Operadores de  Comparacion
+
+edad_luis=15
+edad_martha=30
+
+
+#>Mayor que
+print (edad_luis > edad_martha)
+#< Menor que
+print(edad_luis > edad_martha)
+# == igual que 
+print(edad_luis == edad_martha)
+# != diferente que
+print(edad_luis != edad_martha)
+#and Y
+print (edad_luis > 18 and edad_luis > edad_martha)
+#or  O
+print (edad_luis > 18 or edad_luis > edad_martha)
+#not NO
+print (not edad_luis > 50)
+
+
+
+#Ejercicios 
+edad_eduardo=18
+edad_renato=26
+edad_laura=35
+
+# Como saber si eduardo es mayor de edad
+print(edad_eduardo >= 18)
+# Como saber si eduardo es mayor que laura
+print(edad_eduardo > edad_laura)
+# Como saber si renato es menor que laura pero mayor que eduardo
+print(edad_renato<edad_laura and edad_renato>edad_eduardo)
+# como saber si laura puede ser mayor que renato o menor que eduardo
+print(edad_laura>edad_renato or edad_laura < edad_eduardo)
+
+#Operadores de Asignacion
+# = asignacion
+edad = 50
+# += Incremento
+edad += 1
+# -= Decremento
+edad -= 1
+# *= Multiplicador
+edad *= 1
+# /=division
+edad /= 2 
---
+++ Dia1/04-condicional-if.py
@@ -0,0 +1,22 @@
+edad_roberta = 20
+
+if edad_roberta >=18:
+    print('si se puede entrar a la discoteca')
+else:
+    print('no puedes entrar a la discoteca')
+
+
+edad_martin = 70
+if edad_martin >= 65: 
+    print('esta persona esta jubilada')
+elif edad_martin >= 18:
+    print('Esa persona es mayor de edad')
+elif edad_martin>=0:
+    print('Esa persona es menor de edad')
+else: 
+    print('Edad imposible')
+
+
+#la forma para ingresar valores al programa desde una consola 
+data = input("Hola, ingresa tu nombre : ")
+print(data)
